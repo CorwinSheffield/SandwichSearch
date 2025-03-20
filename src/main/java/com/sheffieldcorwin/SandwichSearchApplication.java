6e0b87858fd7b441d3220d83d43b049d1135c161
Corwin Sheffield <sheffieldcorwin@gmail.com>
2025-03-20 07:40:30 -0400

    Initial commit

---
+++ src/main/java/com/sheffieldcorwin/SandwichSearchApplication.java
@@ -0,0 +1,13 @@
+package com.sheffieldcorwin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandwichSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandwichSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sheffieldcorwin/SandwichSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sheffieldcorwin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandwichSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
