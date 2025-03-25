7085d93194e7765ec83839908c17bd15fa30ef3a
Corwin Sheffield <sheffieldcorwin@gmail.com>
2025-03-25 13:12:14 -0400

    search by name interface method

--- src/main/java/com/sheffieldcorwin/dao/SandwichShopRepository.java
+++ src/main/java/com/sheffieldcorwin/dao/SandwichShopRepository.java
@@ -1,11 +1,12 @@
 package com.sheffieldcorwin.dao;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.ListCrudRepository;
-import org.springframework.stereotype.Repository;
 
 import com.sheffieldcorwin.model.SandwichShop;
 
 public interface SandwichShopRepository extends ListCrudRepository<SandwichShop, Long> {
-
+	public Optional<SandwichShop> findByNameIgnoreCase(String name);
 
 }
